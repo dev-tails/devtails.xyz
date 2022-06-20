0679ca94c9fd34818659841aed765f0995c493d7
Adam Berg <adam@xyzdigital.com>
2022-06-20 16:22:03 -0700

    Add println! for URL

--- adam/code/hyde/src/main.rs
+++ adam/code/hyde/src/main.rs
@@ -5,6 +5,7 @@ use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("http://127.0.0.1:7878");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
